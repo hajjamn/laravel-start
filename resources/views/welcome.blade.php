7dfce74d295d2117352f8a79db6089f1a808fc82
hajjamn <hajjamn@gmail.com>
2024-05-24 09:47:37 +0200

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('title','Laravel')
+
+@section('content')
+
+<main>
+    <div class="container">
+        <h1>Hello, Laravel!</h1>
+    </div>
+</main>
+
+@endsection
